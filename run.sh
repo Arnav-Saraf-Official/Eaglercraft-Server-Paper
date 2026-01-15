2f3036654f4b11c32b846467bdc055db7fdbab14
Arnav Saraf <reacharnavsaraf@gmail.com>
2026-01-14 22:14:25 -0600

    Start server in foreground with echo message
    Removed background execution and added echo statement.

--- run.sh
+++ run.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
-
-# 1. Start the server in the background using '&'
-# We use 'nohup' and redirect output to a file so it doesn't clutter your terminal
-nohup java -Xmx2G -Xms2G -XX:+UseG1GC -XX:+ParallelRefProcEnabled \
-  -XX:+UnlockExperimentalVMOptions -Dcom.mojang.eula.agree=true \
-  -jar paper-1.12.2.jar > server.log 2>&1 &
-
-# 2. Wait a few seconds for the server to actually bind to the port
-sleep 5
-
-# 3. Now run your visibility commands
-for port in $(gh codespace ports --json sourcePort -q '.[].sourcePort'); do
-  gh codespace ports visibility "$port:public" -c "$CODESPACE_NAME"
-done
+echo "Starting"
+java -Xmx2G -Xms2G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -Dcom.mojang.eula.agree=true -jar paper-1.12.2.jar
