f080dfbc7d9ad95824128b52b84b65ec56160b1f
Arnav Saraf <reacharnavsaraf@gmail.com>
2026-01-15 04:33:16 +0000

    try 3

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo "Starting"
+gh codespace ports visibility 25565:public -c "$CODESPACE_NAME"
 java -Xmx2G -Xms2G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -Dcom.mojang.eula.agree=true -jar paper-1.12.2.jar
